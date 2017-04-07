643bb74c7b38cea744ea4551c85e9a2356dad7c3
Gareth Bult <gareth@encryptec.net>
2017-04-08 00:42:32 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='pymamba',
     version='0.1',
-    packages=[''],
-    url='',
+    packages=['lmdb', 'ujson'],
+    url='https://github.com/oddjobz/pymamba',
     license='MIT',
     author='Gareth Bult',
     author_email='oddjobz@linux.co.uk',
