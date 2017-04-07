8ba9005b54be25a1488a9cd68c0e36056f7fc621
Gareth Bult <gareth@encryptec.net>
2017-04-07 22:36:29 +0100

    Adding setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pymamba',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Gareth Bult',
+    author_email='oddjobz@linux.co.uk',
+    description='Database library for Python'
+)
